90f25e882a37a7a07d9d2878b38cccd80fff4e78
Manuel Rojas <mrojasmt@gmail.com>
2011-10-19 16:02:30 -0700

    Added layout links (with routes)

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,11 +12,12 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",  :content => "#{@base_title} | Home")
     end
+    
     it "should have a non-blank body" do
       get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
@@ -42,6 +43,17 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",  :content => "#{@base_title} | About")
+    end
+  end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",  :content => "#{@base_title} | Help")
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,4 +24,9 @@ RSpec.configure do |config|
   # examples within a transaction, comment the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  # webrat configuration
+end
+  Webrat.configure do |config|
+  config.mode = :rails
+
 end
