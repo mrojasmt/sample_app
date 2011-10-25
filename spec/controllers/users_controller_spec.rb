bc4ec828f4dd9cb90b57d2351cf5d35634360b88
Manuel Rojas <mrojasmt@gmail.com>
2011-10-24 22:32:24 -0700

    Finished the password machinery

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -16,7 +16,12 @@ describe UsersController do
     
     it "should find the right user" do
       get :show, :id => @user
-      assigns(:each).should == @user
+      assigns(:user).should == @user
+    end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector("title", :content => @user.name)
     end
   end
 
