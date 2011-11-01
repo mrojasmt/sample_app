ca19e0c4b1e135b67365ba092b7bcad7934d2af3
Manuel Rojas <mrojasmt@gmail.com>
2011-11-01 15:23:24 -0700

    Can now destroy users (as admins)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
   
   def index
     @title = "All users"
@@ -36,12 +37,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      #it worked
       redirect_to @user, :flash => { :success => "Profile updated." }
     else
       @title = "Edit user"
       render 'edit'
     end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path, :flash => { :success => "User Destroyed." }
   end
   
   private
@@ -53,5 +58,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(user)
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -148,7 +148,28 @@ describe User do
       end
     end
   end
+  
+  describe "admin attribute" do
+    
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should respond to admin" do
+      @user.should respond_to(:admin)
+    end
+    
+    it "should not be an admin by deafult" do
+      @user.should_not be_admin
+    end
+    
+    it "should convertable to an admin" do
+      @user.toggle!(:admin)
+      @user.should be_admin
+    end
+  end
 end
+
 
 
 # == Schema Information
@@ -161,5 +182,7 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
